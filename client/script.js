92c52f94afb870691d9fbc666270d6b263a45df3
Safa <shaikhsohanaaz@gmail.com>
2023-01-18 20:11:11 +0530

    ok

--- client/script.js
+++ client/script.js
@@ -82,7 +82,7 @@ const handleSubmit = async (e) => {
   loader(messageDiv);
 
   // send POST request to server
-  const response = await fetch("https://localhost:5001", {
+  const response = await fetch("http://localhost:5001", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
